f8d00250001b721e2ab75433157927f26a741fa3
pautrias <ptrias@fundaciobit.org>
2026-01-30 11:42:02 +0100

    Problema amb plugin de login a pinbaladmin front

--- pinbaladmin-front/src/main/java/org/fundaciobit/pinbaladmin/front/utils/InitServlet.java
+++ pinbaladmin-front/src/main/java/org/fundaciobit/pinbaladmin/front/utils/InitServlet.java
@@ -2,7 +2,7 @@ package org.fundaciobit.pinbaladmin.front.utils;
 
 import org.fundaciobit.pinbaladmin.commons.utils.Constants;
 import org.apache.log4j.Logger;
-
+import org.fundaciobit.genapp.common.i18n.I18NException;
 import org.fundaciobit.genapp.common.web.i18n.I18NUtils;
 import org.fundaciobit.pluginsib.core.v3.utils.PluginsManager;
 import org.fundaciobit.pluginsib.login.api.IPluginLogin;
@@ -17,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * Servlet emprat per inicialitzar el Front
@@ -28,6 +29,15 @@ import java.util.Properties;
 public class InitServlet extends HttpServlet {
 
     protected final Logger log = Logger.getLogger(getClass());
+
+	public static enum TipusPluginLogin{
+		LOGINIB, MOCK
+	}
+
+    public static final String LOGIN_PLUGIN_KEY = Constants.PINBALADMIN_PROPERTY_BASE + "pluginsib.login";
+
+    public static IPluginLogin loginPluginLoginIB= null;
+    public static IPluginLogin loginPluginMock= null;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -43,8 +53,18 @@ public class InitServlet extends HttpServlet {
             log.error("Error inicialitzant el sistema de traduccions web: " + th.getMessage(), th);
         }
 
-        IPluginLogin pl = (IPluginLogin) getPluginLogin();
-        PluginLoginManager.setPluginLogin(pl);
+		try {
+//			boolean debug = true;
+//			TipusPluginLogin loginib = TipusPluginLogin.LOGINIB;
+//			IPluginLogin pl = (IPluginLogin) getPluginLogin(debug, loginib);
+
+			IPluginLogin pl = (IPluginLogin) getPluginLoginOld();
+			PluginLoginManager.setPluginLogin(pl);
+			
+		} catch (ServletException e) {
+			log.error("Error carregant el plugin de login: " + e.getMessage(), e);
+			throw new ServletException("Error carregant el plugin de login: " + e.getMessage(), e);
+		}
 
     }
 
@@ -53,7 +73,7 @@ public class InitServlet extends HttpServlet {
      * @return
      * @throws Exception
      */
-    private IPluginLogin getPluginLogin() throws ServletException {
+    private IPluginLogin getPluginLoginOld() throws ServletException {
 
         Properties properties = new Properties();
         properties.putAll(Configuracio.getSystemAndFileProperties());
@@ -89,7 +109,93 @@ public class InitServlet extends HttpServlet {
 
             return (IPluginLogin) obj;
         }
-
     }
+//    
+//    private IPluginLogin getPluginLogin(boolean debug, TipusPluginLogin tipusPlugin) throws I18NException {
+//    	
+//    	
+//		log.info("Plugin Login: tipusPlugin=" + tipusPlugin);
+//		
+//		IPluginLogin loginPlugin;
+//		switch (tipusPlugin) {
+//			case LOGINIB:
+//				loginPlugin = loginPluginLoginIB;
+//				break;
+//			case MOCK:
+//				loginPlugin = loginPluginMock;
+//				break;
+//			default:
+//				throw new I18NException("plugin.tipus.desconegut");
+//		}
+//		
+//		log.info("loginPlugin inicial: " + loginPlugin);
+//		
+//        if (loginPlugin == null) {
+////            final String propertyPlugin = LOGIN_PLUGIN_KEY;
+//
+//            Properties propTmp = Configuracio.getSystemAndFileProperties();
+//
+//			if (debug) {
+//				log.info("Propietats de sistema i fitxer de configuració:");
+//				Set<Object> set = propTmp.keySet();
+//				for (Object object : set) {
+//					String key = (String) object;
+//					String value = propTmp.getProperty(key);
+//					log.info(key + ": " + value);
+//				}
+//			}
+//            
+//			String propertySuffix;
+//			switch (tipusPlugin) {
+//				case LOGINIB:
+//					propertySuffix = ".loginib";
+//					break;
+//				case MOCK:
+//					propertySuffix = ".mock";
+//					break;
+//				default:
+//					throw new I18NException("plugin.tipus.desconegut");
+//			}
+//			
+//			String clName = LOGIN_PLUGIN_KEY + propertySuffix;
+//			log.info("Property plugin class name: " + clName);
+//			
+//			String className = propTmp.getProperty(clName);
+//            
+//            log.info("className: " + className);
+//            Object pluginInstance = PluginsManager.instancePluginByClassName(className,
+//                    Constants.PINBALADMIN_PROPERTY_BASE, propTmp);
+//            
+//            
+//
+////            Object pluginInstance = PluginsManager.instancePluginByProperty(propertyPlugin,
+////                    Constants.PINBALADMIN_PROPERTY_BASE, propTmp);
+//
+//            if (pluginInstance == null) {
+//            	log.error("No s'ha pogut instanciar el plugin de login amb la classe: " + className);
+//                throw new I18NException("plugin.donotinstantiateplugin.userinfo");
+//            }
+//            
+//            switch (tipusPlugin) {
+//				case LOGINIB:
+//					loginPluginLoginIB = (IPluginLogin) pluginInstance;
+//					loginPlugin = loginPluginLoginIB;
+//					break;
+//				case MOCK:
+//					loginPluginMock = (IPluginLogin) pluginInstance;
+//					loginPlugin = loginPluginMock;
+//					break;
+//				default:
+//					throw new I18NException("plugin.tipus.desconegut");
+//			}
+//        }else {
+//			log.info("loginPlugin ja existeix. " + loginPlugin.getClass().getName());
+//        }
+//        return loginPlugin;    	
+//    	
+//    	
+//    	
+//    }
+
 
 }
