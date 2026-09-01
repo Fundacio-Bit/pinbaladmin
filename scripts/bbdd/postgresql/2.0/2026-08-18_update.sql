d390954dace2b12a5b5c017ef7be9cc9ecfe91d6
anadal-fundaciobit <anadal@fundaciobit.org>
2026-09-01 07:37:04 +0200

    Modificar que el Ajuntaments no estan gestionats per GovernDigital

--- scripts/bbdd/postgresql/2.0/2026-08-18_update.sql
+++ scripts/bbdd/postgresql/2.0/2026-08-18_update.sql
@@ -3,3 +3,8 @@
 ALTER TABLE pad_entitat ADD COLUMN gestionatpergoverndigital boolean NOT NULL DEFAULT false;
 
 UPDATE pad_entitat SET  gestionatpergoverndigital=true WHERE codipinbal is not null and codipinbal <> 'PARLAMENT';
+
+UPDATE pad_entitat SET gestionatpergoverndigital=false WHERE nom like 'Ajuntament%';
+
+
+
